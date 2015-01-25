72fbab4545d8e5481543631f19d517bbd7baf1fc
idugcoal <idugcoal@gmail.com>
2015-01-25 15:27:01 -0800

    rLearnPython
    Projects from r/learnpython

---
+++ 99_Bottles.py
@@ -0,0 +1,10 @@
+for i in range(99, 0, -1):
+	b = "bottles"
+	print "%d %s of beer on the wall" % (i, b)
+	print "%d %s of beer" % (i, b)
+	print "take one down, pass it around"
+	if i - 1 == 1:
+		b = "bottle"
+	else:
+		b = "bottles"
+	print "%d %s of beer on the wall\n" % (i - 1, b)
