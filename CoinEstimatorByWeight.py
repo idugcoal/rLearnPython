d9c47b698c783aa130524f4891c7b9f466e25098
idugcoal <idugcoal@gmail.com>
2015-01-26 01:23:33 -0800

    Coin Estimator by Weight
    Needs a loop to iterate thru rather than individual variables

---
+++ CoinEstimatorByWeight.py
@@ -0,0 +1,19 @@
+"""
+COIN		WEIGHT			NUM PER WRAPPER
+Penny: 		2.500 grams		50
+Nickel:		5.000 grams		40
+Dime:		2.268 grams		50
+Quarter:	5.670 grams		40
+"""
+
+# IDEA: put above data into a list and loop thru it
+
+weight_pennies = int(raw_input("Weight of pennies: "))
+weight_nickels = int(raw_input("Weight of nickels: "))
+weight_dimes = int(raw_input("Weight of dimes: "))
+weight_quarters = int(raw_input("Weight of quarters: "))
+
+num_pennies = weight_pennies / 2.500
+num_nickels = weight_nickels / 5.000
+num_dimes = weight_dimes / 2.268
+num_quarters = weight_quarters / 5.670
