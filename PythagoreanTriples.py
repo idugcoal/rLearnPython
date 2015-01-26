4633a54426e2f833729f1545797cf192ac9b1aff
idugcoal <idugcoal@gmail.com>
2015-01-25 23:49:16 -0800

    Pythagorean Triples

--- PythagoreanTriples.py
+++ PythagoreanTriples.py
@@ -1,12 +1,21 @@
-sides = []
-for i in range(1, 4):
-	print("Enter length of side %d: ") % i
-	user_input = raw_input()
-	sides.append(user_input)
+def triple():
+	sides = []
+	for i in range(1, 4):
+		print("Enter length of side %d: ") % i
+		user_input = raw_input()
+		sides.append(int(user_input))
 
-if(pow(int(sides[0]), 2) + pow(int(sides[1]), 2) == pow(int(sides[2]), 2)):
-	print("Yes, it is a triple.")
-else:
-	print("No, it is not a triple.")
+	if(sides[0]*sides[0] + sides[1]*sides[1] == sides[2]*sides[2]):
+		print("Yes, it is a triple.")
+	else:
+		print("No, it is not a triple.")
+
+	print("Try again? y/n")
+	choice = raw_input()
+	if choice == 'y':
+		triple()
+
+triple()
+
 
 
