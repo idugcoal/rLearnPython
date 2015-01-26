4c4c3815f23f22dff3ca471a7e5e7b3e4828b43b
idugcoal <idugcoal@gmail.com>
2015-01-26 13:42:00 -0800

    Clarifications

--- CoinEstimatorByWeight.py
+++ CoinEstimatorByWeight.py
@@ -1,9 +1,9 @@
 """
-COIN		WEIGHT			NUM PER WRAPPER
-Penny: 		2.500 grams		50
-Nickel:		5.000 grams		40
-Dime:		2.268 grams		50
-Quarter:	5.670 grams		40
+COIN		VALUE	WEIGHT			NUM PER WRAPPER
+Penny: 		.01		2.500 grams		50
+Nickel:		.05		5.000 grams		40
+Dime:		.10		2.268 grams		50
+Quarter:	.25		5.670 grams		40
 """
 
 # IDEA: put above data into a list and loop thru it
--- PythagoreanTriples.py
+++ PythagoreanTriples.py
@@ -1,17 +1,17 @@
 def triple():
-	sides = []
+	sides = []				
 	for i in range(1, 4):
 		print("Enter length of side %d: ") % i
 		user_input = raw_input()
 		sides.append(int(user_input))
 
-	if(sides[0]*sides[0] + sides[1]*sides[1] == sides[2]*sides[2]):
-		print("Yes, it is a triple.")
+	sides.sort()		# moves the hypotenuse t0 sides[2]
+	if sides[0] * sides[0] + sides[1] * sides[1] == sides[2]*sides[2]:
+		print("Yes, it is a Pythagorean triple.")
 	else:
-		print("No, it is not a triple.")
+		print("No, it is not a Pythagorean triple.")
 
-	print("Try again? y/n")
-	choice = raw_input()
+	choice = raw_input("Try again? y/n ")
 	if choice == 'y':
 		triple()
 
